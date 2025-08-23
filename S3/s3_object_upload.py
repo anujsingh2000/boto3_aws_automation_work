c98de06099582e983c3aa305b99e1ddbef79a85b
AnujRathore <sanuj2824@gmail.com>
2025-08-23 22:53:33 +0530

    Updted S3 code

--- S3/s3_object_upload.py
+++ S3/s3_object_upload.py
@@ -24,25 +24,73 @@
 
 ##_________________________________________________________________________________
 
-#S3 object download
+# #S3 object download
 
-import boto3
+# import boto3
 
-s3_client=boto3.client('s3')
+# s3_client=boto3.client('s3')
 
-#Params
-bucket_name='my-boto3-demo-bucket-2025-72511'
-object='list_ec2.py'
-download_path='C:\\Users\\Anuj Rathore\\Desktop\\Me\\downloaded_my_app.txt'
-
-
-response=s3_client.list_objects(Bucket=bucket_name)
-for obj in response.get('Contents', []):
-    print(obj['Key'])
-
-def object_downlaod(bucket_name,object_key,download_path):
-    s3_client.download_file(bucket_name,object_key,download_path)
-    print(f"Object {object_key} downloaded successfully from {bucket_name} bucket.")
+# #Params
+# bucket_name='my-boto3-demo-bucket-2025-72511'
+# object='list_ec2.py'
+# download_path='C:\\Users\\Anuj Rathore\\Desktop\\Me\\downloaded_my_app.txt'
 
 
-object_downlaod(bucket_name,object,download_path)
+# response=s3_client.list_objects(Bucket=bucket_name)
+# for obj in response.get('Contents', []):
+#     print(obj['Key'])
+
+# def object_downlaod(bucket_name,object_key,download_path):
+#     s3_client.download_file(bucket_name,object_key,download_path)
+#     print(f"Object {object_key} downloaded successfully from {bucket_name} bucket.")
+
+
+# object_downlaod(bucket_name,object,download_path)
+
+##_________________________________________________________________________________________
+
+##List Object in a S3 Bucket 
+
+# import boto3
+
+# s3_client=boto3.client('s3')
+
+# bucket_name='my-boto3-demo-bucket-2025-72511'
+
+# response=s3_client.list_objects(Bucket=bucket_name)
+# for objects in response.get('Contents',[]):
+#     print(objects['Key'])
+
+##_____________________________________________________
+
+## Delete object from S3 bucket
+
+# import boto3
+# s3_client=boto3.client('s3')
+
+# bucket_name='my-boto3-demo-bucket-2025-72511'
+# object_key='list_ec2.py'
+
+# response=s3_client.delete_object(Bucket=bucket_name,Key=object_key)
+# print(f"Deleted {object_key}")
+
+##____________________________________________________________________
+
+## Upload object with metadata
+
+# import boto3
+
+# s3_client = boto3.client('s3')
+
+# bucket_name ='my-boto3-demo-bucket-2025-72511'
+# object_key = 'config.txt'
+# file_path = 'C:\\Users\\Anuj Rathore\\Desktop\\Me\\data.json'  # Local file
+
+# # Upload with metadata
+# s3_client.upload_file(
+#     Filename=file_path,
+#     Bucket=bucket_name,
+#     Key=object_key,
+#     ExtraArgs={"Metadata": {"project": "healthcheck", "env": "dev"}}
+# )
+# print("Uploaded with metadata!")
